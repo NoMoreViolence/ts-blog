27f05a7d3a6dc1446034ee5219c53b55e0d7918a
NoMoreViolence <ljh86029926@gmail.com>
2018-03-18 22:38:25 +0900

    Mobx 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
+// 리액트 라우터 돔 사용 URL 전환 위해
+// import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import * as React from 'react';
+// mobx 모듈
+// 오토바인드
+import autobind from 'autobind-decorator';
+import { observer, inject } from 'mobx-react';
 import './App.css';
+// 인덱스에서 만들어진 스토어를 온다, Mobx의 좋은 점은 리덕스와 달리 스토어를 한 개로 제한하지 않는다. 모든 것을 한 Store에 밀어넣을 필요가 없어졌다
+import { Store } from './index';
+// 컴포넌트 추가
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
 
-const logo = require('./logo.svg');
+// Store를 생성 할 때는 앞에 물음표를 붙인다
+interface Props {
+  store?: Store;
+}
 
-class App extends React.Component {
+@inject('store')
+@observer
+class App extends React.Component<Props, {}> {
   render() {
+    const store = this.props.store as Store;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Header />
+        <Content />
+        <p>{JSON.stringify(store.test)}</p>
+        <button onClick={this.click}>추가</button>
       </div>
     );
+  }
+  @autobind
+  click() {
+    const store = this.props.store as Store;
+    store.add();
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,28 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+// Mobx 쓰기ㅡ위해 추가
+import { observable, action, configure } from 'mobx';
+import { Provider } from 'mobx-react';
+// useStrict(true) 대신에 이걸 사용하라고 업데이트가 되서 이걸 사용중, 중요한건 이 구문을 왜 적는지 모르겠다....
+configure({ enforceActions: true });
+
+// 임시 연습용 스토어 추가
+export class Store {
+  @observable test: number[] = [];
+  @action
+  add(): void {
+    this.test.push(Math.round(Math.random() * 100));
+  }
+}
+
+const store = new Store();
+
+// 렌더링
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
