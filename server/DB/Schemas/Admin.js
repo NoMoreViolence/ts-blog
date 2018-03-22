3fba816c5ba834c21998f5592f1cd4045ec00d3f
NoMoreViolence <ljh86029926@gmail.com>
2018-03-22 23:22:11 +0900

    레거시 파일은 없어져버려

--- server/DB/Schemas/Admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  password: { type: String, required: true, trim: true },
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Admin', AdminSchema);
