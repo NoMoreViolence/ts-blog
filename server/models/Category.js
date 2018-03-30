f1a4d49fd00fe8d51eafc188f937fe623e9e3fa7
NoMoreViolence <ljh86029926@gmail.com>
2018-03-30 10:21:13 +0900

    카테고리 rest api 작업 거의 완료됨

--- server/models/Category.js
+++ server/models/Category.js
@@ -3,16 +3,21 @@ const Schema = mongoose.Schema;
 
 // 카테고리 스키마
 const Category = new Schema({
-  category: { type: String, required: true, unique: true }, // 카테고리 이름, 필수, 유니크함, 공백을 없애버리지~
+  category: { type: String, required: true, unique: true }, // 카테고리 이름, 필수, 유니크함, 공백을 없앰
   posts: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'post' }] // 포스트 populate 사용 하기 위해서 ref로 포스트들 저장될 디비를 가리킴
 });
 
-/* */
 // 카테고리 중복 체크
 Category.statics.findSameCategory = function(category) {
   return this.findOne({ category }).exec();
 };
-// 카테고리 생성!
+
+// 전체 카테고리 출력
+Category.statics.findAll = function() {
+  return this.find({});
+};
+
+// 카테고리 생성
 Category.statics.createCategory = function(category) {
   const Cart = new this({
     category
@@ -20,11 +25,18 @@ Category.statics.createCategory = function(category) {
 
   return Cart.save();
 };
-/* */
 
-// 카테고리 삭제 부분
-Category.statics.remove = function(category) {
-  return this.remove({ category });
+// 카테고리 변경
+Category.statics.changeCategory = function(category, changeCategory) {
+  return this.findOneAndUpdate(
+    { category: category },
+    { category: changeCategory }
+  );
+};
+
+// 카테고리 삭제
+Category.statics.deleteCategory = function(category) {
+  return this.findOneAndRemove({ category });
 };
 
 // 익스포트
--- server/routes/api/category/category.controller.js
+++ server/routes/api/category/category.controller.js
@@ -1,10 +1,54 @@
 const Category = require('./../../../models/Category');
 
 /*
-    POST /api/auth
+    GET /api/category/all
     {
-        username,
-        password
+
+    }
+*/
+// 모든 카테고리만 검색 후 출력
+exports.all = (req, res) => {
+  const show = () => {
+    return Category.findAll();
+  };
+
+  // respond to the client
+  const respond = result => {
+    res.json({
+      success: true,
+      message: 'View All Categories',
+      category: result
+    });
+  };
+
+  // 에러가 생겼을 때
+  const onError = error => {
+    res.status(409).json({
+      success: false,
+      message: error.message
+    });
+  };
+
+  show() // 실행
+    .then(respond) // 응답
+    .catch(onError); // 에러
+};
+/*
+    GET /api/category/picked/:CartName
+    {
+
+    }
+*/
+// 특정 카테고리 정보 가져오기, 카테고리의 포스트 전부를 가져오게 할 것임 || (아직 완성되지 않음) => 포스트 rest api 정리가 끝나지 않았음
+exports.picked = (req, res) => {
+  console.log(req.params.CartName);
+};
+
+/*
+    POST /api/category/create
+    {
+        token,
+        category
     }
 */
 // 카테고리 생성
@@ -15,13 +59,13 @@ exports.create = (req, res) => {
   const create = exists => {
     console.log(exists);
     if (exists) {
-      throw new Error('category exists');
+      throw new Error('Category Exists');
     } else {
       return Category.createCategory(category); // 카테고리 생성
     }
   };
 
-  // respond to the client
+  // 응답
   const respond = () => {
     res.json({
       success: true,
@@ -37,9 +81,100 @@ exports.create = (req, res) => {
     });
   };
 
-  // 카테고리 별 작업
+  Category.findSameCategory(category) // 중복 카테고리 찾기
+    .then(create) // 중복 카테고리가 없으면 새 카테고리 생성 OR 만들지 않음
+    .then(respond) // 상황에 따라 응답
+    .catch(onError); // 오류 처리
+};
+
+/*
+    PUT /api/category/change
+    {
+        token,
+        category,
+        changeCategory
+    }
+*/
+// 카테고리 변경
+exports.change = (req, res) => {
+  const { category, changeCategory } = req.body;
+
+  // 카테고리 삭제 부분
+  const change = exists => {
+    if (exists) {
+      console.log(`${exists.category} will be changeed to ${changeCategory}`);
+      // 카테고리 변경
+      return Category.changeCategory(category, changeCategory); // 카테고리 삭제
+    } else {
+      // 삭제할 카테고리가 없음
+      throw new Error('Category does not exist - Category.delete()');
+    }
+  };
+
+  // 응답
+  const respond = () => {
+    res.json({
+      success: true,
+      message: `Change Category Success => ${category} to ${changeCategory}`,
+      result: changeCategory
+    });
+  };
+
+  // 에러가 생겼을 때
+  const onError = error => {
+    res.status(409).json({
+      success: false,
+      message: error.message
+    });
+  };
+
   Category.findSameCategory(category)
-    .then(create)
+    .then(change)
+    .then(respond)
+    .catch(onError);
+};
+
+/*
+    DELETE /api/category/delete
+    {
+        token,
+        category
+    }
+*/
+// 카테고리 삭제
+exports.delete = (req, res) => {
+  const { category } = req.body;
+
+  // 카테고리 삭제 부분
+  const remove = exists => {
+    console.log(`${exists.category} will be deleted`);
+    if (exists) {
+      // 카테고리 삭제
+      return Category.deleteCategory(category); // 카테고리 삭제
+    } else {
+      // 삭제할 카테고리가 없음
+      throw new Error('Category does not exist - Category.delete()');
+    }
+  };
+
+  // 응답
+  const respond = () => {
+    res.json({
+      success: true,
+      message: 'Delete Category Success'
+    });
+  };
+
+  // 에러가 생겼을 때
+  const onError = error => {
+    res.status(409).json({
+      success: false,
+      message: error.message
+    });
+  };
+
+  Category.findSameCategory(category)
+    .then(remove)
     .then(respond)
     .catch(onError);
 };
