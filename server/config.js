57551cb47bb7dabfe489e66f11296058478d2218
NoMoreViolence <ljh86029926@gmail.com>
2018-04-26 04:32:52 +0900

    리덕스 사용으로 종료

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   secret: 'FuckYouThisIsSecretKey',
   mongodbUri:
-    'mongodb://LeonardoLee:jihoon8033@ds117739.mlab.com:17739/ts-blog-database'
+    'mongodb://LeonardoLee:jihoon8033@ds117739.mlab.com:17739/ts-blog-database',
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
+
 import App from './components/App';
 
-const Root = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const Root = () => <App />;
 
 export default Root;
